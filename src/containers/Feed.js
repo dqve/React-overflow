a8dd4af020f2c195e3e4996ac53e25744c627901
Ayoola David <ayooladavid@gmail.com>
2020-06-16 02:48:57 +0100

    initial commit

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Card from '../components/Card/Card';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import Card from '../components/Card/Card'
 
 const FeedWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   flex-direction: column;
-  margin: 5%;
-`;
+  margin: 5%;`
 
-const Alert = styled.div`
-  text-align: center;
-`;
+const Alert = styled.div`text-align: center;`
 
-const ROOT_API = 'https://api.stackexchange.com/2.2/';
+const ROOT_API = 'https://api.stackexchange.com/2.2/'
 
 class Feed extends Component {
   constructor() {
@@ -22,27 +20,27 @@ class Feed extends Component {
       data: [],
       loading: true,
       error: '',
-    };
+    }
   }
 
   async componentDidMount() {
     try {
       const data = await fetch(
         `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`,
-      );
+      )
       const dataJSON = await data.json();
 
       if (dataJSON) {
         this.setState({
           data: dataJSON,
           loading: false,
-        });
+        })
       }
     } catch (error) {
       this.setState({
         loading: false,
         error: error.message,
-      });
+      })
     }
   }
 
@@ -50,17 +48,19 @@ class Feed extends Component {
     const { data, loading, error } = this.state;
 
     if (loading || error) {
-      return <Alert>{loading ? 'Loading...' : error}</Alert>;
+      return <Alert>{loading ? 'Loading...' : error}</Alert>
     }
 
     return (
       <FeedWrapper>
         {data.items.map(item => (
-          <Card key={item.question_id} data={item} />
+          <Link key={item.question_id} to={`/questions/${item.question_id}`}>
+          <Card data={item} />
+          </Link>
         ))}
       </FeedWrapper>
-    );
+    )
   }
 }
 
-export default Feed;
+export default Feed
