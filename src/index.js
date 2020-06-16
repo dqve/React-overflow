c5261c74a2f4fa8259085fb6554b912b2db7c60c
Ayoola David <ayooladavid@gmail.com>
2020-06-16 16:17:45 +0100

    update components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+ import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+
+ReactDOM.hydrate(
+	<App/>
+, document.getElementById('root'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
